67e7ed3047e22134b86950438d6b80f137cf2392
shaudai <shaudai0402@hotmail.com>
2016-12-28 23:10:02 +0800

    Setup app Layouts, SeekBar, TextView, and Button

--- app/src/main/java/com/garfield_chou/eggtimer/MainActivity.java
+++ app/src/main/java/com/garfield_chou/eggtimer/MainActivity.java
@@ -2,12 +2,57 @@ package com.garfield_chou.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void controlTimer(View view) {
+
+        Log.i("Button pressed", "Pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        final TextView timerTextView = (TextView) findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int minutes = (int) progress / 60;
+                int seconds = progress - minutes * 60;
+
+                String secondString = Integer.toString(seconds);
+
+                if (seconds == 0) {
+                    secondString = "00";
+                }
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
